35d6b987804531ccb02b7b51d5b273c16bb41d94
Yuriy Schevchuck <yuriy.shevchuck@gmail.com>
2020-12-14 21:29:05 +0200

    initial commit

---
+++ Create.sql
@@ -0,0 +1,23 @@
+USE [master]
+
+IF EXISTS(SELECT name FROM sys.databases WHERE name = 'dbo.PEX')
+ DROP DATABASE [dbo.PEX]
+
+CREATE DATABASE [dbo.PEX]
+
+USE [dbo.PEX]
+
+CREATE TABLE [dbo].[Transaction](
+ [UserID] [nvarchar](40) NOT NULL,
+ [VendorID] [nvarchar](40) NOT NULL,
+ [TransactionAmount] [bigint] NOT NULL,
+ [TransactionDate] [datetime] NOT NULL
+)
+
+CREATE TABLE [dbo].[Vendor](
+ [VendorID] [nvarchar](40) NOT NULL,
+ [VendorName] [nvarchar](40) NOT NULL,
+ [MonthlyPerUserCap] [bigint] NOT NULL,
+ [MonthlyCap] [bigint] NOT NULL,
+ [Enabled] [bit] NOT NULL
+)
